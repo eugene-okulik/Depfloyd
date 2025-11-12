1cb5e3593da38ffcf9aabe0a4275f47981bbd893
Nikolay Makarchuk <makarchuk93@gmail.com>
2025-11-12 15:41:47 +0300

    added second test

--- homework/nick_makarchuk/Homework_26/task.py
+++ homework/nick_makarchuk/Homework_26/task.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
-from time import sleep
 
 
 @pytest.fixture()
@@ -20,14 +19,13 @@ def driver():
 
 def test_tab(driver):
     driver.get('http://testshop.qa-practice.com/')
-    wait = WebDriverWait(driver, 5)
+    wait = WebDriverWait(driver, 10)
     item = driver.find_element(By.CSS_SELECTOR, 'a[href="/shop/customizable-desk-9"]')
     actions = ActionChains(driver)
     actions.key_down(Keys.CONTROL)
     actions.click(item)
     actions.key_up(Keys.CONTROL)
     actions.perform()
-    sleep(3)
     tabs = driver.window_handles
     driver.switch_to.window(tabs[1])
     add_to_cart = driver.find_element(By.ID, 'add_to_cart')
@@ -35,4 +33,27 @@ def test_tab(driver):
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.btn-secondary')))
     button_continue = driver.find_element(By.CSS_SELECTOR, 'button.btn-secondary')
     button_continue.click()
-    sleep(3)
+    wait.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, 'div.modal.show')))
+    driver.close()
+    driver.switch_to.window(tabs[0])
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href="/shop/cart"]')))
+    cart = driver.find_element(By.CSS_SELECTOR, 'a[href="/shop/cart"]')
+    cart.click()
+    wait.until(EC.url_contains('/shop/cart'))
+    wait.until(EC.presence_of_element_located((By.XPATH, "//h6[contains(text(), 'Customizable Desk')]")))
+    order_item = driver.find_element(By.XPATH, "//h6[contains(text(), 'Customizable Desk')]")
+    assert order_item.text == 'Customizable Desk (Steel, White)'
+    print(order_item.text)
+
+
+def test_popup(driver):
+    driver.get('http://testshop.qa-practice.com/')
+    wait = WebDriverWait(driver, 10)
+    actions = ActionChains(driver)
+    busket_button = driver.find_element(By.CSS_SELECTOR, 'a.btn.btn-primary.a-submit')
+    actions.move_to_element(busket_button)
+    actions.click(busket_button)
+    actions.perform()
+    item = driver.find_element(By.CSS_SELECTOR, 'strong.product-name.product_display_name')
+    assert item.text == '[FURN_0096] Customizable Desk (Steel, White)'
+    print(item.text)
